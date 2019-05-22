95d6f8561d80019dac2f8d4dfe2eed38243cd4f5
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 04:05:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,22 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each do |name|
+    badge_message = badge_maker(name)
+    messages.push(badge_message)
+  end
+
+  return messages
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |name, index|
+    assignment = "Hello, #{name}! You'll be assigned to room #{index}!"
+    room_assignments.push(assignment)
+  end
+  return room_assignments
+end
